9f6fd04b89dd370180857bc7a499189bc2c7187a
gfyfaye <125503468+gfyfaye@users.noreply.github.com>
2025-09-21 00:57:48 -0500

    styles for courselist

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -10,13 +10,26 @@ interface CourseListProps {
 }
 
 const CourseList = ({ courses }: CourseListProps ) => (
-  <ul>
-  {Object.entries(courses).map(([id, course]) => (
-      <li key={id}>
-        {course.term} CS {course.number}: {course.title} ({course.meets})
-      </li>
-    ))}
-  </ul>
+
+    <div className= "grid grid-cols-1 sm: grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 p-4">
+    {Object.entries(courses).map(([id, course]) => (
+        <div key={id} className = "border-3 border-gray-200 rounded-lg p-8 h-58 flex flex-col justify-between">
+            <div className = "text-2xl">
+            {course.term} CS {course.number}
+            </div>
+
+            <div className = "text-md">
+             {course.title}
+            </div>
+
+            <div className="border-t border-gray-200 my-2"></div>
+
+            <div>
+                {course.meets}
+            </div>
+        </div>
+        ))}
+    </div>
 );
 
 export default CourseList;
